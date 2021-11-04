e64e6e6c4cf02a78d6bc214918c01dd05328bb4a
Collin Chan <collinchan@DESKTOP-RBMQEU2.hitronhub.home>
2021-11-03 19:29:42 -0700

    periodic update

--- public/jsx/mainscript.jsx
+++ public/jsx/mainscript.jsx
@@ -1,13 +1,16 @@
 const RootDisplay = require('./rootdisplay.jsx');
 
-/*
-const sidebarLinks = document.querySelectorAll("#sidebar button");
+const sidebarLinks = document.querySelectorAll("#sidebar span");
 for (let link of sidebarLinks) {
   link.addEventListener('click', (event) => {
+    prevLink = document.querySelector("#sidebar .active");
+    prevLink.setAttribute("class", "");
+    event.target.setAttribute("class", "active");
 
+    const rootNode = document.querySelector('#root');
+    ReactDOM.render(<RootDisplay />, rootNode);
   });
 }
-*/
 
 const rootNode = document.querySelector('#root');
 ReactDOM.render(<RootDisplay />, rootNode);
