8d6a79618df8c2c575f7d4f3cef0ce3f660d84bb
Collin Chan <collinchan@DESKTOP-RBMQEU2.hitronhub.home>
2021-11-07 02:34:46 -0800

    add loading logic

--- public/jsx/article.jsx
+++ public/jsx/article.jsx
@@ -3,11 +3,28 @@ const ReactDOM = require('react-dom');
 const Tooltip = require('./tooltip.jsx');
 
 class Article extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState((state) => ({
+      display: !state.display
+    }));
+  }
+
   render() {
+    let style = {
+      display: (this.state.display) ? "" : "none"
+    };
     return (
-      <div>
+      <div style={style}>
         <Tooltip title={this.props.title} text={this.props.tooltipText}>
-          <img src={this.props.imageUrl} onClick={() => {this.props.displayVideoHandler(this.props.videoUrl)}}/>
+          <img src={this.props.imageUrl} onClick={() => {this.props.displayVideoHandler(this.props.videoUrl)}} onLoad={this.toggle} />
         </Tooltip>
         <span className="header"> {this.props.title} </span>
         <span className="footer"> {this.props.company} </span>
